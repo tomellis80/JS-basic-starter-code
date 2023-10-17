85cec27714370b8ccc3d6c2444e0d3b7efe4e2c2
Tom Ellis <130678444+tomellis80@users.noreply.github.com>
2023-10-17 15:47:46 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var timeOfDay = [6, "noon", 8, "morning", "evening", 12];
+timeOfDay.forEach(function (time, index) {
+  console.log(`The ${time} element is at position ${index}.`);
+});
+// The 6 element is at position 0.
+// The noon element is at position 1.
+// The 8 element is at position 2.
+// The morning element is at position 3.
+// The evening element is at position 4.
+// The 12 element is at position 5.
